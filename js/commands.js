e6225def08dba50a24bcd1791e64e1315161f7ae
Arpit-Khanulia <88695197+Arpit-Khanulia@users.noreply.github.com>
2023-05-17 23:40:42 +0530

    first commit

---
+++ js/commands.js
@@ -0,0 +1,131 @@
+var youtube = "https://www.youtube.com/channel/UC1CtpaQepwYhlq3cq2pdRhg";
+var twitter = "https://twitter.com/arpit_khanulia";
+var password = "narutouzumaki";
+var linkedin = "https://www.linkedin.com/in/arpit-khanulia-1a6205200/";
+var instagram = "https://www.instagram.com/fire_dr4gon/";
+var github = "https://github.com/Arpit-Khanulia";
+var email = 'mailto:arpitkhanulia7@gmail.com';
+
+whois = [
+    "<br>",
+    '<span class= \"command3\">Arpit Khanulia, </span>',
+    "also known by the alias 'firedragon,' is a B.Tech student at Quantum University with ",
+    'expertise in <span class= \"command3\"> data structures and algorithms. </span>',
+    "                                                                                      ", 
+    "With over <span class= \"command3\">500+ problems</span>  solved across platforms such as <span class= \"command3\">LeetCode, GeeksforGeeks, and Codechef</span> ", 
+    "he has a strong foundation in problem-solving skills.", 
+    "                                                                                      ", 
+    "Arpit has also developed skills in <span class= \"command3\"> web development using Node.js and Express</span>,", 
+    "building several projects like a <span class= \"command3\">daily journal and to-do list</span> .",
+    "                                                                                      ", 
+    "With a passion for learning and an ability to quickly grasp new concepts,", 
+    "Arpit is a valuable asset to any team.",
+    "Arpit is passionate about expanding his knowledge and skills.",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">youtube/firedr4g0n' + "</a>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/arpit_khanulia' + '</a>',
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/arpit-khanulia' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/fire_dr4gon' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/Arpit-Khanulia' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">how can you become a superuser in linux? </span>',
+            //    Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+ls = [
+    "<br>",
+    "password.txt",
+    "<br>"
+];
+
+lsa = [
+    "<br>",
+    "password.txt",
+    '<span class=\"command\">do_not_open.txt<span > ',
+    "<br>"
+];
+
+catpass = [
+    "<br>",
+    "Password is not here fool !",
+    "may be you should ask for password directly",
+    "<br>"
+];
+
+catdo = [
+    "<br>",
+    "permission denied!",
+    "<br>"
+];
+sudocatdo = [
+    "<br>",
+    "Oh! you are clever, you should become an inspector",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Arpit Khanulia?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">video</span>          View YouTube videos',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">FiREDR4G0N Not A Corporation. All knights reserved.</span>',
+    ",                                                                                             ",
+    "7MM===YMM db  `7MM'''Mq.  `7MM'''YMM  `7MM'''Yb. `7MM'''Mq.             .g8'''bgd           `7MN.   `7MF'",
+    " MM    `        MM   `MM.   MM    `7    MM    `Yb. MM   `MM.          .dP'     `M             MMN.    M  ",
+    " MM     `7MM    MM   ,M9    MM   d      MM     `Mb MM   ,M9       ,AM dM'       `   ,pP''Yq.  M YMb   M  ",
+    " MM''MM   MM    MMmmdM9     MMmmMM      MM      MM MMmmdM9       AVMM MM           6W'    `Wb M  `MN. M  ",
+    " MM   Y   MM    MM  YM.     MM   Y  ,   MM     ,MP MM  YM.     ,W' MM MM.    `7MMF'8M      M8 M   `MM.M  ",
+    " MM       MM    MM   `Mb.   MM     ,M   MM    ,dP' MM   `Mb. ,W'   MM `Mb.     MM  YA.    ,A9 M     YMM  ",
+    "JMML.   .JMML..JMML. .JMM..JMMmmmmMMM .JMMmmmdP' .JMML. .JMM.AmmmmmMMmm `'bmmmdPY   `Ybmmd9'.JML.    YM  ",
+    "                                                                    MM                                    ",
+    "                                                                    MM                                    ",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+startbanner = [
+    '<span class=\"color2\"> Mounting <span > <span class= \"command2\">/home/visitor... <span>',
+    '<span class="index">FiREDR4G0N Not A Corporation. All knights reserved.</span>',
+    "                                                                                             ",
+    "Welcome to my interactive web terminal                                                                                             ",
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+                                                       
+
+                                                                                                          
